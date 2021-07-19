4ec0f24485c3debdd69ffd241b3d2cb3f8d9e715
Aniket Kumar <aniketsinha101@gmail.com>
2021-07-19 12:43:11 +0530

    Create first_repeating.cpp

---
+++ Inside_Code/first_repeating.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+char firstrepeating(string str)
+{
+  unordered_set <char> h;
+  for(int i=0;i<int(str.length());i++)
+  {
+    char ch=str[i];
+    if(h.find(ch)!=h.end())
+    {
+      return ch;
+    }
+    else
+    {
+      h.insert(ch);
+    }
+  }
+  return '\0';
+}
+int main()
+{
+  string str="aniketnohara";
+  cout<<firstrepeating(str)<<endl;
+  return 0;
+}
