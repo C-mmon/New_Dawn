a74b61d07ba552d6d1df0602f42f178f68d25e40
Aniket Kumar <aniketsinha101@gmail.com>
2021-08-09 23:33:38 +0530

    Create perfect_square.cpp

---
+++ Leetcode/perfect_square.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) 
+    {
+        
+        long low=1;
+        long high=num;
+        long mid;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(mid*mid==num)
+            {
+                return true;
+            }
+            else if(mid*mid>num)
+            {
+             high=mid-1;   
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+};
