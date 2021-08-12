9678ca2471cae7ad735959085101b35945cb58ce
Aniket Kumar <aniketsinha101@gmail.com>
2021-08-12 19:07:35 +0530

    Create subset_.cpp

---
+++ Leetcode/subset_.cpp
@@ -0,0 +1,21 @@
+void sub(vector <int> &nums, int i, vector <int> temp)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+            return ;
+        }
+        sub(nums,i+1,temp);
+        temp.push_back(nums[i]);
+        sub(nums,i+1,temp);
+            
+    }
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector <int> temp;
+        sub(nums,0,temp);
+        return ans;
+        
+    
+        
+    }
